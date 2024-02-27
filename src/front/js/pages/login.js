1a5d4122fdcaead557129f83231831228f2ec2a9
Sabrina Estévez <sesmo.dev@gmail.com>
2024-02-27 19:14:26 +0000

    inicio y cierre de sesión implementados. Falta crear ruta privada y creación de usuario nuevo

---
+++ src/front/js/pages/login.js
@@ -0,0 +1,31 @@
+import React, { useContext, useState } from "react";
+import { Context } from "../store/appContext";
+import rigoImageUrl from "../../img/rigo-baby.jpg";
+import "../../styles/home.css";
+import { useNavigate } from "react-router-dom";
+
+export const Login = () => {
+    const { store, actions } = useContext(Context);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        actions.login(email, password);
+    };
+
+    if(store.token && store.token != "" && store.token != undefined) navigate("/");
+
+    return (
+        <div className="text-center mt-5">
+            <h1>LOGIN</h1>
+            {(store.token && store.token != "" && store.token != undefined) ? "You are already logged in" + store.token :
+            <div>
+                <input type="text" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <input type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <button onClick={handleClick}>Login</button>
+            </div>
+            }
+        </div>
+    );
+};
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,6 +1,7 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
+			token: null,
 			message: null,
 			demo: [
 				{
@@ -21,34 +22,74 @@ const getState = ({ getStore, getActions, setStore }) => {
 				getActions().changeColor(0, "green");
 			},
 
-			getMessage: async () => {
+			syncTokenFromLocalStorage: () => {
+				const token = localStorage.getItem("token");
+				if(token && token !="" && token != undefined) setStore({token: token});
+			},
+
+			login: async (email, password) => {
+				const options = {
+					method: 'POST',
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({
+						"email": email,
+						"password": password
+					})
+				};
+
 				try{
+					const response = await fetch(`https://ominous-goldfish-v6v7pwp9jj9qcwvrq-3001.app.github.dev/api/token`, options)
+					if (response.status !== 200) {
+						alert("There has been an error");
+						return false;
+					}
+					const data = await response.json();
+					localStorage.setItem("token", data.access_token)
+					setStore({ token: data.access_token })
+					return true;
+				}
+				catch(error){
+					console.error("There has been an error login in");
+				}
+			},
+
+			logout: () => {
+				localStorage.removeItem("token");
+				setStore({token: null});
+			}, 
+			
+			getMessage: async () => {
+				try {
 					// fetching data from the backend
 					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
 					const data = await resp.json()
 					setStore({ message: data.message })
 					// don't forget to return something, that is how the async resolves
 					return data;
-				}catch(error){
+				} catch (error) {
 					console.log("Error loading message from backend", error)
 				}
 			},
+			
 			changeColor: (index, color) => {
 				//get the store
 				const store = getStore();
-
+				
 				//we have to loop the entire demo array to look for the respective index
 				//and change its color
 				const demo = store.demo.map((elm, i) => {
 					if (i === index) elm.background = color;
 					return elm;
 				});
-
+				
 				//reset the global store
 				setStore({ demo: demo });
 			}
 		}
-	};
+	}
 };
+
 
 export default getState;
